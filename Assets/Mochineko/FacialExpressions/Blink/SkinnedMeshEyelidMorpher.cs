4fd496b759458cd6f52ef62eb718cf91e028712f
mochi-neko <t.o.e.4315@gmail.com>
2023-08-09 17:46:04 +0900

    Improve skinned mesh eyelid morpher for both sample

--- Assets/Mochineko/FacialExpressions/Blink/SkinnedMeshEyelidMorpher.cs
+++ Assets/Mochineko/FacialExpressions/Blink/SkinnedMeshEyelidMorpher.cs
@@ -11,23 +11,38 @@ namespace Mochineko.FacialExpressions.Blink
     {
         private readonly SkinnedMeshRenderer skinnedMeshRenderer;
         private readonly IReadOnlyDictionary<Eyelid, int> indexMap;
+        private readonly bool separateBoth;
 
         /// <summary>
         /// Creates a new instance of <see cref="SkinnedMeshEyelidMorpher"/>.
         /// </summary>
         /// <param name="skinnedMeshRenderer">Target renderer.</param>
         /// <param name="indexMap">Map of eyelid and blend shape index.</param>
+        /// <param name="separateBoth">Whether separate both eyelids blend shape.</param>
         public SkinnedMeshEyelidMorpher(
             SkinnedMeshRenderer skinnedMeshRenderer,
-            IReadOnlyDictionary<Eyelid, int> indexMap)
+            IReadOnlyDictionary<Eyelid, int> indexMap,
+            bool separateBoth = false)
         {
             this.skinnedMeshRenderer = skinnedMeshRenderer;
             this.indexMap = indexMap;
+            this.separateBoth = separateBoth;
         }
 
         public void MorphInto(EyelidSample sample)
         {
-            if (indexMap.TryGetValue(sample.eyelid, out var index))
+            if (separateBoth && sample.eyelid == Eyelid.Both)
+            {
+                if (indexMap.TryGetValue(Eyelid.Left, out var rightIndex))
+                {
+                    skinnedMeshRenderer.SetBlendShapeWeight(rightIndex, sample.weight * 100f);
+                }
+                if (indexMap.TryGetValue(Eyelid.Right, out var leftIndex))
+                {
+                    skinnedMeshRenderer.SetBlendShapeWeight(leftIndex, sample.weight * 100f);
+                }
+            }
+            else if (indexMap.TryGetValue(sample.eyelid, out var index))
             {
                 skinnedMeshRenderer.SetBlendShapeWeight(index, sample.weight * 100f);
             }
